c924070d97c57149e5cc9116f7090152ad3cd0d0
Anna Sudol <sudolann@gmail.com>
2024-02-19 19:38:28 +0100

    Updated Checkout, Accordion

--- app/accordion/index.tsx
+++ app/accordion/index.tsx
@@ -1,25 +1,45 @@
 import { useState } from "react";
 import { ITaskItem } from "@/app/types";
+import CheckoutInput from "@/app/checkoutInput";
 type IAccordionProps = {
   title: string;
   tasks: ITaskItem[];
 };
-const Accordion: React.FC<IAccordionProps> = ({ title }) => {
+const Accordion: React.FC<IAccordionProps> = ({ title, tasks }) => {
   const [expanded, setExpanded] = useState(false);
   const toggleExpanded = () => setExpanded((current) => !current);
-
+  // console.log(tasks, "tasks");
+  const handleChange=(e)=> {
+    const tasksItem = tasks.filter((t) => t.description === e.target.value)[0]
+      .checked;
+      console.log(tasks, "t")
+  }
   return (
     <div
-      className="my-2 sm:my-4 md:my-6 shadow-sm cursor-pointer bg-white"
+      // className="my-2 sm:my-4 md:my-6 shadow-sm cursor-pointer bg-white"
       onClick={toggleExpanded}
     >
       <div className="px-6 text-left items-center h-20 select-none flex justify-between flex-row">
         <h5 className="flex-1">{title}</h5>
         {/* <div className="flex-none pl-2">{expanded ? minusIcon : plusIcon}</div> */}
+        <form className="flex flex-col">
+          {tasks.map((task, index) => {
+            const id = `${index}${task.description}`;
+            return (
+              <CheckoutInput
+                identifier={id}
+                key={id}
+                title={task.description}
+                onChange={handleChange}
+                defaultChecked={
+                  tasks.filter((t) => t.description === task.description)[0]
+                    .checked
+                }
+              />
+            );
+          })}
+        </form>
       </div>
-      {/* <div className={`px-6 pt-0 overflow-hidden transition-[max-height] duration-500 ease-in ${expanded ? "max-h-40" : "max-h-0"}`}>
-     
-      </div> */}
     </div>
   );
 };
--- app/checkoutInput/index.tsx
+++ app/checkoutInput/index.tsx
@@ -1,4 +1,31 @@
-const checkoutInput = () => {
-  return <div>check</div>;
+import React, { ChangeEvent, FC } from "react";
+import cn from "classnames";
+
+interface Props  {
+  identifier: string;
+  title: string;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  defaultChecked: boolean;
+}
+
+const CheckoutInput: FC<Props> = ({
+  identifier,
+  defaultChecked,
+  title,
+  onChange,
+}) => {
+  return (
+    <label>
+      <input
+        type="checkbox"
+        value={title}
+        id={identifier}
+        onChange={onChange}
+        checked={defaultChecked}
+      />
+      {title}
+    </label>
+  );
 };
-export default checkoutInput;
+
+export default CheckoutInput;
