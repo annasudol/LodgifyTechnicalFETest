f8c0ac6fdea2ad506d000ed4062d85f46ec9843f
Anna Sudol <sudolann@gmail.com>
2024-02-21 21:19:29 +0100

    checkbox updated

--- app/checkoutInput/index.tsx
+++ app/checkoutInput/index.tsx
@@ -16,7 +16,7 @@ const CheckoutInput: FC<Props> = ({ identifier, checked, title, onChange }) => {
       >
         <input
           type="checkbox"
-          className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-customGrey-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-gray-900 checked:before:bg-gray-900"
+          className="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-md border border-customGrey-200 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-successGreen checked:bg-successGreen checked:before:bg-successGreen"
           id={identifier}
           value={identifier}
           onChange={onChange}
@@ -24,18 +24,19 @@ const CheckoutInput: FC<Props> = ({ identifier, checked, title, onChange }) => {
         />
         <span className="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
           <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill="none"
             xmlns="http://www.w3.org/2000/svg"
-            className="h-3 w-3"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            stroke="currentColor"
-            stroke-width="1"
           >
+            <rect width="16" height="16" rx="4" fill="#00B797" />
             <path
               fill-rule="evenodd"
-              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
               clip-rule="evenodd"
-            ></path>
+              d="M6.49574 10.1007L3.888 7.30201L3 8.24832L6.49574 12L13 4.94631L12.1182 4L6.49574 10.1007Z"
+              fill="white"
+            />
           </svg>
         </span>
       </label>
